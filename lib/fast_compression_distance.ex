ece96733948e777a225139fc62a81a01189286b0
Taylor Redden <taylor.redden@interfolio.com>
2021-09-14 10:02:53 -0500

    add fast compression distance comparing

--- lib/fast_compression_distance.ex
+++ lib/fast_compression_distance.ex
@@ -1,18 +1,33 @@
 defmodule FastCompressionDistance do
-  @moduledoc """
-  Documentation for `FastCompressionDistance`.
-  """
+  alias FastCompressionDistance.LZW
 
-  @doc """
-  Hello world.
+  def compare(subject, target), do: run_compression_and_comparison(subject, target)
 
-  ## Examples
+  defp run_compression_and_comparison(subject, target) do
+    {_, subject_table} = LZW.encode(subject)
+    {_, target_table} = LZW.encode(target)
 
-      iex> FastCompressionDistance.hello()
-      :world
+    subject_keys = Map.keys(subject_table)
+    target_keys = Map.keys(target_table)
 
-  """
-  def hello do
-    :world
+    subject_key_count = Enum.count(subject_keys)
+    intersection_key_count = Enum.count(intersection(subject_keys, target_keys))
+
+    result = 1 - (subject_key_count - intersection_key_count) / subject_key_count
+
+    metadata = %{
+      subject_key_count: subject_key_count,
+      intersection_key_count: intersection_key_count
+    }
+
+    {:ok, {result, metadata}}
+  end
+
+  defp intersection(subject_keys, target_keys) do
+    MapSet.intersection(
+      Enum.into(subject_keys, MapSet.new()),
+      Enum.into(target_keys, MapSet.new())
+    )
+    |> MapSet.to_list()
   end
 end
