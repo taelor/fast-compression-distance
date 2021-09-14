ffd2b95b68bf078cab739d27abb66479dfc49ad4
Taylor Redden <taylor.redden@interfolio.com>
2021-09-14 09:44:09 -0500

    initialize repo

---
+++ lib/fast_compression_distance.ex
@@ -0,0 +1,18 @@
+defmodule FastCompressionDistance do
+  @moduledoc """
+  Documentation for `FastCompressionDistance`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> FastCompressionDistance.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/fast_compression_distance_test.exs
@@ -0,0 +1,8 @@
+defmodule FastCompressionDistanceTest do
+  use ExUnit.Case
+  doctest FastCompressionDistance
+
+  test "greets the world" do
+    assert FastCompressionDistance.hello() == :world
+  end
+end
